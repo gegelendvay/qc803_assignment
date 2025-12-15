9e5658c826d445d31fbcbe7aa11ae85359ca7f7f
Gergely Lendvay <28988586+gegelendvay@users.noreply.github.com>
2025-12-15 22:02:36 +0100

    feat: Convert work to FP structure

--- src/main.py
+++ src/main.py
@@ -1,30 +1,38 @@
 # https://quantum.cloud.ibm.com/learning/en/courses/foundations-of-quantum-error-correction/correcting-quantum-errors/shor-code
 import sys
-import random
 from qiskit_aer import AerSimulator
 from qiskit import QuantumCircuit, ClassicalRegister
 
 backend = AerSimulator()
 
+DATA_QUBITS = list(range(9))
+BLOCKS = [(0,1,2), (3,4,5), (6,7,8)]
+ANCILLAS_Z = [(9,10), (11,12), (13,14)]
+ANCILLAS_X = [15,16]
+
 # 9 data qubits + 6 ancilla qubits for Z-type syndrome + 2 ancilla qubits for X-type syndrome
-for s in range(int(sys.argv[1])):
+def create_circuit():
     qc = QuantumCircuit(17)
-    blocks = [(0,1,2), (3,4,5), (6,7,8)]
-    ancillas_z = [(9,10), (11,12), (13,14)]
-    ancillas_x = [15,16]
 
     # classical registers for syndrome measurement outcomes
     cr_z = []
     for i in range(3):
-        cr_z.append(ClassicalRegister(2, f'cr_z{i}'))
-        qc.add_register(cr_z[i])
+        register = (ClassicalRegister(2, f'cr_z{i}'))
+        qc.add_register(register)
+        cr_z.append(register)
+
     cr_x = ClassicalRegister(2, 'cr_x')
     qc.add_register(cr_x)
 
-    # -- encoding --
+    result = ClassicalRegister(1, 'logical_result')
+    qc.add_register(result)
+    return qc, cr_z, cr_x, result
+
+def encode_qubit(qc):
     # spreads logical qubit to the first qubit of each block
     qc.cx(0,3)
     qc.cx(0,6)
+
     for i in [0,3,6]:
         # apply Hadamard to transform phase-flip to bit-flip
         qc.h(i)
@@ -34,33 +42,27 @@ for s in range(int(sys.argv[1])):
 
     qc.barrier()
 
-    # -------------------------------------
-    # -- error introduction --
-    '''r = random.random()
-    q = random.randint(0,8)
-    if r < 1/3:
-        qc.x(q)
-    elif r < 2/3:
-        qc.z(q)
+def inject_error(qc, index):
+    if index < 9:
+        qc.x(index)
+    elif index < 18:
+        qc.z(index-9)
     else:
-        qc.y(q)'''
-    if s in range(9):
-        qc.x(s)
-    elif s in range(9,18):
-        qc.z(s-9)
-    else:
-        qc.y(s-18)
-    qc.barrier()
-    # -------------------------------------
+        qc.y(index-18)
 
+    qc.barrier()
+
+def measure_z_syndrome(qc, cr_z):
     # -- Z-type syndrome measurement for X errors (bit flips) --
-    for i, block in enumerate(blocks):
-        a1, a2 = ancillas_z[i]
+    for i, block in enumerate(BLOCKS):
+        a1, a2 = ANCILLAS_Z[i]
         cbits_z = cr_z[i]
+
         # S1 = Z_a Z_b
         qc.cx(block[0], a1)
         qc.cx(block[1], a1)
         qc.measure(a1, cbits_z[0])
+
         # S2 = Z_b Z_c
         qc.cx(block[1], a2)
         qc.cx(block[2], a2)
@@ -68,38 +70,44 @@ for s in range(int(sys.argv[1])):
 
     qc.barrier()
 
+def measure_x_syncdome(qc, cr_x):
     # -- X-type syndrome measurement for Z errors (phase flips) --
     # HXH = Z
-    for i in range(9):
+    for i in DATA_QUBITS:
         qc.h(i)
+
     # S1 = X0 X1 X2 X3 X4 X5
     for i in range(6):
-        qc.cx(i, ancillas_x[0])
-    qc.measure(ancillas_x[0], cr_x[0])
+        qc.cx(i, ANCILLAS_X[0])
+    qc.measure(ANCILLAS_X[0], cr_x[0])
+
     # S2 = X3 X4 X5 X6 X7 X8
     for i in range(3, 9):
-        qc.cx(i, ancillas_x[1])
-    qc.measure(ancillas_x[1], cr_x[1])
+        qc.cx(i, ANCILLAS_X[1])
+    qc.measure(ANCILLAS_X[1], cr_x[1])
+
     # reverse HXH
-    for i in range(9):
+    for i in DATA_QUBITS:
         qc.h(i)
 
     qc.barrier()
     '''for i in range(9,17):
         qc.reset(i)'''
 
+def correct_bit_flips(qc, cr_z):
     # -- bit-flip correction (X errors) using Z-type syndromes --
-    for i, block in enumerate(blocks):
+    for i, block in enumerate(BLOCKS):
         q0, q1, q2 = block
         with qc.if_test((cr_z[i], 0b01)):
             qc.x(q0)
-        with qc.if_test((cr_z[i], 0b11)):
-            qc.x(q2)
         with qc.if_test((cr_z[i], 0b10)):
             qc.x(q1)
+        with qc.if_test((cr_z[i], 0b11)):
+            qc.x(q2)
 
     qc.barrier()
 
+def correct_phase_flips(qc, cr_x):
     # -- phase-flip correction (Z errors) using X-type syndromes --
     with qc.if_test((cr_x, 0b01)):
         qc.z(0)
@@ -110,25 +118,46 @@ for s in range(int(sys.argv[1])):
 
     qc.barrier()
 
+def decode_qubit(qc):
     # -- decoding -- (don't understand CCX)
     for i in [0,3,6]:
         qc.cx(i, i+1)
         qc.cx(i, i+2)
         qc.ccx(i+1, i+2, i)
         qc.h(i)
+
     qc.cx(0,3)
     qc.cx(0,6)
     qc.ccx(3,6,0)
-    result = ClassicalRegister(1, 'logical_result')
-    qc.add_register(result)
+
+def measure(qc, result):
     qc.measure(0, result[0])
 
+def build_circuit(index):
+    qc, cr_z, cr_x, result = create_circuit()
 
-    # simulation
-    job = backend.run(qc, shots=1).result()
-    counts = job.get_counts()
+    encode_qubit(qc)
+    inject_error(qc, index)
+    measure_z_syndrome(qc, cr_z)
+    measure_x_syncdome(qc, cr_x)
+    correct_bit_flips(qc, cr_z)
+    correct_phase_flips(qc, cr_x)
+    decode_qubit(qc)
+    measure(qc, result)
+
+    return qc
+
+def run_simulation(qc, shots=1):
+    backend = AerSimulator()
+    job = backend.run(qc, shots=shots).result()
+    return job.get_counts()
+
+for s in range(int(sys.argv[1])):
+    qc = build_circuit(s)
+    counts = run_simulation(qc)
+
     if next(iter(counts))[0] == '1':
-        print(counts)
+        print(s, counts)
 
     '''bitstring = next(iter(counts))
     parts = bitstring.split()
